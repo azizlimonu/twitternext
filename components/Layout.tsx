adeee367028a09c3c4956b19fdd1075253ff69f1
Aziz Limonu <azizlimonu@gmail.com>
2023-03-29 13:51:57 +0800

    create sidebar

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -3,6 +3,7 @@ interface LayoutProps {
 }
 
 import React from "react";
+import FollowBar from "./layout/FollowBar";
 import Sidebar from "./layout/Sidebar";
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
@@ -10,11 +11,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="h-screen bg-black">
       <div className="container h-full mx-auto xl:px-10 max-w-5xl">
         <div className="grid grid-cols-4 h-full">
-          {/* sidebar */}
           <Sidebar />
           <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
             {children}
           </div>
+          <FollowBar />
         </div>
       </div>
     </div>
--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { BiLogOut } from "react-icons/bi";
 import { BsHouseFill, BsBellFill } from "react-icons/bs";
 import { FaUser } from "react-icons/fa";
+import SidebarItem from "./SidebarItem";
+import SidebarLogo from "./SidebarLogo";
+import SidebarTweetButton from "./SidebarTweetButton";
 
 const Sidebar = () => {
   const items = [
@@ -21,7 +24,7 @@ const Sidebar = () => {
       icon: FaUser,
       label: "Profile",
       // href: `/users/${currentUser?.id}`,
-      href:'/users/123',  
+      href: "/users/123",
       auth: true,
     },
   ];
@@ -30,11 +33,21 @@ const Sidebar = () => {
     <div className="col-span-1 h-full pr-4 md:pr-6">
       <div className="flex flex-col items-end">
         <div className="space-y-2 lg:w-[230px]">
-          {/* sidebarLogo */}
+          <SidebarLogo />
+          {items?.map((item) => (
+            <SidebarItem
+              key={item.href}
+              // alert={item.alert}
+              // auth={item.auth}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+          <SidebarItem icon={BiLogOut} label="Logout" onClick={() => { }} />
+          <SidebarTweetButton />
         </div>
-
       </div>
-      <div>Sidebar</div>
     </div>
   );
 };

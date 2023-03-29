54db368be6ecbcfe71512202a25f04ea61e47ae2
Aziz Limonu <azizlimonu@gmail.com>
2023-03-29 16:03:28 +0800

    create modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import Layout from "@/components/Layout";
+import Modal from "@/components/Modal";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
+    <>
+      <Modal isOpen title="test modal" />
+      <Layout>
+        <Component {...pageProps} />;
 
-    <Component {...pageProps} />;
-
-    </Layout>
+      </Layout>
+    </>
   )
 }

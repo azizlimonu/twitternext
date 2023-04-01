19383edfa3a1c2d930a8c34f4d744cf20132025d
Aziz Limonu <azizlimonu@gmail.com>
2023-04-01 15:01:04 +0800

    create user page

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,7 +9,7 @@ import Sidebar from "./layout/Sidebar";
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
     <div className="h-screen bg-black">
-      <div className="container h-full mx-auto xl:px-10 max-w-5xl">
+      <div className="container h-full mx-auto xl:px-10 max-w-full">
         <div className="grid grid-cols-4 h-full">
           <Sidebar />
           <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
--- pages/api/users/[userId].ts
+++ pages/api/users/[userId].ts
@@ -2,6 +2,16 @@ import { NextApiRequest, NextApiResponse } from "next";
 
 import prisma from '@/libs/prismadb';
 
+// function exclude<User, Key extends keyof User>(
+//   user: User,
+//   keys: Key[]
+// ): Omit<User, Key> {
+//   for (let key of keys) {
+//     delete user[key]
+//   }
+//   return user
+// }
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "GET") {
     return res.status(405).end();
@@ -13,12 +23,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (!userId || typeof userId !== 'string') {
       throw new Error('Invalid User ID');
     }
+    // check if the userId exist in db
     const existingUser = await prisma.user.findUnique({
       where: {
         id: userId
-      }
+      },
     });
-    
+    // const userWithoutPassword = exclude(existingUser, ['hashedPassword'])
+    // check an calculate number of records
     const followersCount = await prisma.user.count({
       where: {
         followingIds: {
